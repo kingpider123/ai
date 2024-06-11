42332c8af1cab6b334250ccce9c1d96f7d52fed9
unknown <phuc25102003@gmail.com>
2024-06-11 10:36:56 +0800

    new

--- homework/8/cartPole.py
+++ homework/8/cartPole.py
@@ -3,20 +3,28 @@ import gymnasium as gym
 env = gym.make("CartPole-v1", render_mode = "human")
 observation, info = env.reset(seed = 42)
 steps = 0
-
-for _ in range(200):
+inrange,_,_,angle_velo=observation
+for _ in range(1000):
     env.render()
-
-    if observation[2] > 0:
-            action = 1    
+    if inrange>=2.24:
+            action=0
+    elif inrange<=-2.24:
+            action=1
     else:
-            action = 0
+            if angle_velo > 0:
+                action = 1    
+            else:
+                action = 0
+       
+
     print("space: ",observation[0])
     
     print("vel: ",observation[1])  
     print("ang: ",observation[2])   
     print("angVel: ",observation[3])
+    
     observation, reward, terminated, truncated, info = env.step(action)
+    inrange,_,_,angle_velo=observation
     steps += 1
     print("step", steps)
 
